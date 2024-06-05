d21ec5413694e62835479b362507c7551c12ed53
Andrew Thomas <athomas94@gmail.com>
2024-06-04 21:05:37 -0400

    inital commit with packages for sharing

---
+++ back_end/node_modules/dnd-character-sheets/dist/Components/AttackTable.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+declare function AttackTable(props: {
+    onChange: (arg0: any, arg1: any) => void;
+    name: string;
+    value?: any;
+    classes?: string;
+    rows: number;
+}): JSX.Element;
+export default AttackTable;
