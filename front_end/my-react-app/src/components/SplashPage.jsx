ae8df2ddad9199af12f979671e84ded6875b2549
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-06-05 15:16:12 -0400

    Bootstrap implemented for splash screen, need custom styling

--- front_end/my-react-app/src/components/SplashPage.jsx
+++ front_end/my-react-app/src/components/SplashPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import '../index.scss';
+import campfireGif from '../assets/Campfire2D.gif'
 
 const SplashPage = () => {
     return (
-        <div className="splash-page">
-            <h1>Greetings, Traveler</h1>
-            <div className="buttons">
-                <button onClick={() => alert('Log In Clicked')}>Log In</button>
-                <button onClick={() => alert('Sign Up Clicked')}>Sign Up</button>
-            </div>
-        </div>
+        <Container className="splash-page text-center">
+            <Row className="justify-content-center">
+                <Col md={8}>
+                    <h1>Greetings, Traveler</h1>
+                    <img src={campfireGif} alt="pixel campfire" className="img-fluid my-4" />
+                    <div className="buttons">
+                        <button onClick={() => alert('Log In Clicked')}>Log In</button>
+                        <button onClick={() => alert('Sign Up Clicked')}>Sign Up</button>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
