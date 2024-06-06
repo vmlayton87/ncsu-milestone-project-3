e947dfa5ec0146fa21c0508223f883935cdabbd6
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-06-05 20:56:55 -0400

    Created signup form with basic styling

--- front_end/my-react-app/src/App.jsx
+++ front_end/my-react-app/src/App.jsx
@@ -1,14 +1,16 @@
+// Dependencies import
 import React from 'react'
 import { useState } from 'react'
 import './App.css'
 
 // Components import
 import SplashPage from './components/SplashPage.jsx'
+import Signup from './components/Signup.jsx'
 
 function App() {
   return (
     <div className="App">
-      <SplashPage />
+      <Signup />
     </div>
   )
 }
