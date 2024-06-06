5603eda2a57e8c0dbe739f8f3f442e6f2f2dd0a8
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-06-05 20:21:30 -0400

    Added artist credits in alt tag

--- front_end/my-react-app/src/components/SplashPage.jsx
+++ front_end/my-react-app/src/components/SplashPage.jsx
@@ -7,7 +7,7 @@ const SplashPage = () => {
     return (
         <Container fluid className="splash-page text-center">
             <h1>Greetings, Traveler</h1>
-            <img src={campfireGif} alt="pixel campfire" className="img-fluid" />
+            <img src={campfireGif} alt="pixel campfire by dustdfg, https://simon-develop.itch.io/2d-campfire-free" className="img-fluid" />
             <div className="buttons">
                 <Button variant="primary" size="lg" className="my-2">Log In</Button>
                 <Button variant="secondary" size="lg" className="my-2">Sign Up</Button>
