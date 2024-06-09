d55ec2f6b0940a22497c8a771090b621d8d650c5
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-06-09 09:57:19 -0400

    Home button in Nav redirects to dashboard

--- front_end/my-react-app/src/components/Navigation.jsx
+++ front_end/my-react-app/src/components/Navigation.jsx
@@ -11,7 +11,7 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/dashboard">Home</Nav.Link>
                 <Nav.Link href="/campaigns">Campaigns</Nav.Link>
             </Nav>
             <Nav className="ms-auto">
