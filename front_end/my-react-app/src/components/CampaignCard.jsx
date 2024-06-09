9fc493d75587fdbc5985e2fb8aef0e3e873de038
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-06-08 20:18:18 -0400

    Added CampaignCard component, fixed double navbar on Dashboard, added images to CampaignCards along with animation.

--- front_end/my-react-app/src/components/CampaignCard.jsx
+++ front_end/my-react-app/src/components/CampaignCard.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import '../index.scss';
 
-// The useHistory hook gives access to the history instance that can be used to navigate programmatically within the App, without needing to use <Link>. It also provides methods to manipulate the history stack (go back/go forward), and can pass state through Navigation, useful for transferring data between Routes. Explanation from chatGPT.
 
 const CampaignCard = ({ campaign }) => {
     const navigate = useNavigate();
@@ -11,15 +10,13 @@ const CampaignCard = ({ campaign }) => {
     }
 
     return (
-        <div className="campaign-cards">
-            <div className="card-container" onClick={handleClick}>
-                <div className="card">
-                    <div className="card-front">
-                        <h3>{campaign.name}</h3>
-                    </div>
-                    <div className="card-back">
-                        <p>{campaign.description}</p>
-                    </div>
+        <div className="card-container" onClick={handleClick}>
+            <div className="card">
+                <div className="card-front" style={{ backgroundImage: `url(${campaign.image})`}}>
+                    <h3>{campaign.name}</h3>
+                </div>
+                <div className="card-back">
+                    <p>{campaign.description}</p>
                 </div>
             </div>
         </div>
--- front_end/my-react-app/src/components/Dashboard.jsx
+++ front_end/my-react-app/src/components/Dashboard.jsx
@@ -4,42 +4,47 @@ import '../index.scss';
 import Navigation from "./Navigation";
 import CampaignCard from "./CampaignCard";
 
+// Import images
+import image1 from '../assets/cardimage-1.jpg'
+import image2 from '../assets/cardimage-2.jpg'
+import image3 from '../assets/cardimage-3.jpg'
+
 const Dashboard = () => {
 
     const campaigns = [
         { 
             id: 1,
             name: 'Campaign 1',
-            description: 'This is a brief description of Campaign number 1.'
+            description: 'This is a brief description of Campaign number 1.',
+            image: image1
         },
         { 
             id: 2,
             name: 'Campaign 2',
-            description: 'This is an exciting description of Campaign number 2.'
+            description: 'This is an exciting description of Campaign number 2.',
+            image: image2
         },
         { 
             id: 3,
             name: 'Campaign 3',
-            description: 'This is a colorful description of Campaign number 3.'
+            description: 'This is a colorful description of Campaign number 3.',
+            image: image3
         },
     ]
 
 
     return (
         <div className="dashboard">
-            <Navigation />
             <Container>
-                <Row className="my-4">
+                <Row>
                     <Col>
                         <h2>Welcome to the Dashboard</h2>
                         <p>Here you can manage your Dungeons & Dragons campaigns.</p>
                     </Col>
                 </Row>
-                <Row>
+                <Row className="cards-row">
                     {campaigns.map(campaign => (
-                    <Col key={campaign.id} xs={12} sm={6} md={4}>
-                        <CampaignCard campaign={campaign} />
-                    </Col>
+                        <CampaignCard key={campaign.id} campaign={campaign} />
                     ))}
                 </Row>
             </Container>
