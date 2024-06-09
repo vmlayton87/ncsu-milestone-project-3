ecfdcc7949433caaf3c3c3e8d65e9ed82f6b2203
Andrew Thomas <athomas94@gmail.com>
2024-06-09 14:07:22 -0400

    Added a character sheet tsx file that reads json and adds it in

--- front_end/my-react-app/src/App.jsx
+++ front_end/my-react-app/src/App.jsx
@@ -7,13 +7,14 @@ import { BrowserRouter as Router, Route, Routes, useLocation  } from 'react-rout
 // Components import
 import './App.css'
 import SplashPage from './components/SplashPage.jsx'
-import DmCard from './components/DmCard.jsx'
 import Signup from './components/Signup.jsx'
 import Login from './components/Login.jsx'
 import Dashboard from './components/Dashboard.jsx'
 import ConditionalNavigation from './components/ConditionalNavigation.jsx'
 import Campaigns from './components/Campaigns.jsx'
 import DmDashboard from './components/DmDashboard.jsx'
+import CharacterSheet from './components/CharacterSheet.jsx'
+import CharacterSheetTSX from './components/CharacterSheet.tsx'
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dmdashboard" element={<DmDashboard />} />
+        <Route path="/charactersheet" element={<CharacterSheetTSX />} />
         <Route path="/campaigns" element={<Campaigns />} /> {/* Ensure this component exists */}
         <Route path="/logout" element={() => {
           // Handle logout logic here
