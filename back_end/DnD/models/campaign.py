1e0fa1f01ca6a185bbfab226fcac2e44c1df53df
CoderKaiHan <hankaicoder@gmail.com>
2024-06-09 14:12:19 -0400

    Updated characters and campaigns models

--- back_end/DnD/models/campaign.py
+++ back_end/DnD/models/campaign.py
@@ -9,6 +9,9 @@ class Campaign(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(250))
     dm = db.Column(db.Integer, db.ForeignKey('users.id'))
+    name = db.Column(db.String(250))
+    description = db.Column(db.TEXT)
+    image_url = db.Column(db.String(250))
     
     
     #define relationship to Note
