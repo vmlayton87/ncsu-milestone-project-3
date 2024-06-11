9c5ceffb62206b9f1f6218165e672a89c3b3f6eb
CoderKaiHan <hankaicoder@gmail.com>
2024-06-11 15:20:25 -0400

    Added username & email check at back end auth route

--- back_end/DnD/blueprints/auth.py
+++ back_end/DnD/blueprints/auth.py
@@ -24,12 +24,31 @@ auth_bp = Blueprint('auth', __name__, url_prefix = '/auth')
 def register():
     try:
       data = request.get_json()
+
+      #check if user exists
+      existing_username = User.query.filter_by(user_name=data['username']).first()
+      if existing_username:
+        return jsonify({'error': 'Username already exists'}), 400
+      
+      #check if email exists
+      existing_email = User.query.filter_by(email=data['email']).first()
+      if existing_email:
+        return jsonify({'error': 'Email already exists'}), 400
+      
+      #hash the password
       hashed_password = bcrypt.generate_password_hash(data['password'], 12)
+
+      #create new user
       new_user = User(user_name=data['username'], email=data['email'], hashed_password=hashed_password)
+
+      #add and commit new user to db
       db.session.add(new_user)
       db.session.commit()
+
+      #return success
       return jsonify({'message': 'User registered successfully'})
     except Exception as err:
+      #return error
       print(f"Error: {err}")
       return jsonify({'error': 'Failed to register user'}), 500
 
