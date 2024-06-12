e5cd65df323b9258ac4455dc9547731750ba0785
CoderKaiHan <hankaicoder@gmail.com>
2024-06-12 17:09:19 -0400

    Added logics at front end to check if a user's token is expired

--- front_end/my-react-app/src/components/protectedRoute.jsx
+++ front_end/my-react-app/src/components/protectedRoute.jsx
@@ -1,13 +1,11 @@
 // Protected Route to check if user is logged in
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { getToken } from '../utils/auth';
+import { tokenExists, isTokenExpired } from '../utils/auth';
 
 // eslint-disable-next-line react/prop-types
 const ProtectedRoute = ({ children }) => {
-    const token = getToken();
-
-    if (!token) {
+    if (!tokenExists() || isTokenExpired()) {
         return <Navigate to="/" />;
     }
 
@@ -16,10 +14,9 @@ const ProtectedRoute = ({ children }) => {
 
 // eslint-disable-next-line react/prop-types
 const PublicRoute = ({ children }) => {
-    const token = getToken();
     const location = useLocation();
 
-    if (token &&(location.pathname === '/' || location.pathname === '/login' || location.pathname === '/signup')) {
+    if (tokenExists() && !isTokenExpired() &&(location.pathname === '/' || location.pathname === '/login' || location.pathname === '/signup')) {
         return <Navigate to="/dashboard" />;
     }
 
