a0a2de998d723cfb9f826585660e5692b919b7bf
CoderKaiHan <hankaicoder@gmail.com>
2024-06-13 13:49:17 -0400

    Merged with NessMergeAll and removed some unnecessary validation  code in Signup page

--- back_end/app.py
+++ back_end/app.py
@@ -3,3 +3,7 @@ from DnD import create_app
 
 # creating an instance called app from the defined function in DnD/__init__.py
 app = create_app()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
